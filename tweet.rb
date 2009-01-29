68b3f435c34d2bb0e04d339e1a56e7a6ec9d48fc
James Stewart <james@saigon.config>
2009-01-29 08:26:57 +0000

    Make our tweets a bit more varied, handling periods of quiet

--- tweet.rb
+++ tweet.rb
@@ -13,7 +13,21 @@ DataMapper.setup(:default, "sqlite3://#{this_path}/flights.db")
 gem 'twitter'
 require 'twitter'
 
-update = "Hello. It's #{DateTime.now.strftime("%H:%M on %A")}. In the past hour I've watched #{Flight.departed.in_past_hour.count} flights take off, and #{Flight.arrived.in_past_hour.count} planes have come to visit"
+hourly_take_offs = Flight.departed.in_past_hour.count
+hourly_arrivals = Flight.arrived.in_past_hour.count
+two_hourly_take_offs = Flight.departed.since(Time.now - 7200).count
+two_hourly_arrivals = Flight.arrived.since(Time.now - 7200).count
+time_and_day = DateTime.now.strftime("%H:%M on %A")
+update = "Hello. It's #{time_and_day}"
+  
+if hourly_take_offs > 0 or hourly_arrivals > 0
+  if hourly_take_offs == two_hourly_take_offs and hourly_arrivals == two_hourly_arrivals
+    update += " and I'm waking up"
+  end
+  update += ". In the past hour I've watched #{hourly_take_offs} flights take off, and #{hourly_arrivals} planes have come to visit"
+elsif two_hourly_take_offs > 0 and two_hourly_arrivals > 0
+  update += " and I'm getting some rest. No take offs or landings in the past hour"
+end
 
 begin
   Twitter::Base.new(ARGV[0], ARGV[1]).update(update)
