36eed64c0f95e9ff3a2504015418eb8c49df5545
James Stewart <james@saigon.config>
2009-01-30 10:10:16 +0000

    Be more careful about whether to send overnight tweets

--- tweet.rb
+++ tweet.rb
@@ -21,18 +21,23 @@ two_hourly_take_offs = Flight.departed.since(Time.now - 7200).count
 two_hourly_arrivals = Flight.arrived.since(Time.now - 7200).count
 time_and_day = DateTime.now.strftime("%H:%M on %A")
 update = "Hello. It's #{time_and_day}"
-  
+send_update = false
+
 if hourly_take_offs > 0 or hourly_arrivals > 0
   if hourly_take_offs == two_hourly_take_offs and hourly_arrivals == two_hourly_arrivals
     update += " and I'm waking up"
   end
   update += ". In the past hour I've watched #{hourly_take_offs} flights take off, and #{hourly_arrivals} planes have come to visit"
+  send_update = true
 elsif two_hourly_take_offs > 0 and two_hourly_arrivals > 0
   update += " and I'm getting some rest. No take offs or landings in the past hour"
+  send_update = true
 end
 
-begin
-  Twitter::Base.new(ARGV[0], ARGV[1]).update(update)
-rescue Twitter::CantConnect
-  puts "Couldn't connect to twitter. Check username/password"
+if send_update
+  begin
+    Twitter::Base.new(ARGV[0], ARGV[1]).update(update)
+  rescue Twitter::CantConnect
+    puts "Couldn't connect to twitter. Check username/password"
+  end
 end
