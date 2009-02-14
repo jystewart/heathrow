3022888a4501cdbe913d386c2eba0768a21419a3
James Stewart <james@saigon.config>
2009-02-14 13:02:55 +0000

    Change init.rb to just load our dependencies. Move data gathering to a separate script

--- init.rb
+++ init.rb
@@ -1,26 +1,9 @@
 #!/usr/bin/ruby
 
 require 'rubygems'
-require File.dirname(__FILE__) + '/lib/flight.rb'
-require File.dirname(__FILE__) + '/lib/heathrow.rb'
+require 'datamapper'
 
+Dir.glob(File.join(File.dirname(__FILE__), 'lib/*.rb')).each { |f| require f  }
 DataMapper.setup(:default, "sqlite3://#{File.dirname(__FILE__)}/flights.db")
-Flight.auto_migrate!
 
-# TODO: Create a FlightNumber if this is a new one
-# TODO: Flesh out FlightNumber and Airport data where appropriate
-def update_flight_data(type)
-  Heathrow::Scraper.fetch_array(type).each do |flight_data|
-    flight = Flight.first(:identifier => flight_data.identifier) || Flight.new
-
-    %W(identifier number terminal due_at status_name status_time type origin).each do |attr|
-      flight.send("#{attr}=", flight_data.send(attr))
-    end
-
-    flight.save
-  end
-  
-end
-
-update_flight_data('arrival')
-update_flight_data('departure')
+HEATHROW_ROOT = File.dirname(__FILE__)
---
+++ scripts/gather_flight_data.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+require File.dirname(__FILE__) + '/../init.rb'
+
+def update_flight_data(type)
+  Heathrow::Scraper.fetch_array(type).each do |flight_data|
+    flight = Flight.first(:identifier => flight_data.identifier) || Flight.new
+
+    %W(identifier number terminal due_at status_name status_time type origin).each do |attr|
+      flight.send("#{attr}=", flight_data.send(attr))
+    end
+
+    flight.save
+  end
+  
+end
+
+update_flight_data('arrival')
+update_flight_data('departure')
