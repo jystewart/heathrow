288a82aba0862c679b9ec211fa73296c352330a1
James Stewart <james@jystewart.vm.bytemark.co.uk>
2009-02-16 14:23:18 +0000

    Prefix 'flight' to google queries to help refine them a bit

--- lib/google_flight.rb
+++ lib/google_flight.rb
@@ -5,7 +5,7 @@ class GoogleFlight
   attr_accessor :origin, :destination
   
   def self.get(flight_number)
-    doc = Hpricot(open("http://www.google.com/search?q=#{flight_number}"))
+    doc = Hpricot(open("http://www.google.com/search?q=Flight%20#{flight_number}"))
     codes = doc.search('div#res ol li div.g').inner_text.match(/\((.+?)\) to .+? \((.+?)\)/)
     if codes and codes.length >= 3
       result = new
